130836bdcf705b9d310810e4b6e6c1414f5e2c29
Gökay <gokayyildirim@web.de>
2023-12-15 18:30:16 +0100

    walk.sh vault login methods

--- check-certificate/walk.sh
+++ check-certificate/walk.sh
@@ -1,11 +1,28 @@
 #!/bin/bash
-
-if [ -z "${VAULT_TOKEN}" ] || [ -z "${VAULT_ADDR}" ]; then
-	echo "VAULT_TOKEN and VAULT_ADDR must be specified."
-	exit 253
+if [ -n "${VAULT_ADDR}" ]; then
+	if [ -z "$VAULT_TOKEN" ]; then
+		if [ -s "$HOME/.vault_tokens" ]; then
+			VAULT_TOKEN=$(awk -v RS= 'NR=1{gsub(/\n/, "", $1); print $1}' "$HOME/.vault_tokens")
+			[[ $? ]] && export VAULT_TOKEN
+		elif [ -n "$VAULT_ROLE_ID" ] && [ -n "$VAULT_SECRET_ID" ]; then
+			VAULT_TOKEN=$(vault write -field=token auth/approle/login role_id="$VAULT_ROLE_ID" secret_id="$VAULT_SECRET_ID")
+			[[ $? ]] && export VAULT_TOKEN
+		elif [ -s "$HOME/.role_id" ] && [ -s "$HOME/.secret_id" ]; then
+			role_id=$(awk -v RS= 'NR=1{gsub(/\n/, "", $1); print $1}' "$HOME/.role_id")
+			secret_id=$(awk -v RS= 'NR=1{gsub(/\n/, "", $1); print $1}' "$HOME/.secret_id")
+			VAULT_TOKEN=$(vault write -field=token auth/approle/login role_id="$role_id" secret_id="$secret_id")
+			[[ $? ]] && export VAULT_TOKEN
+		else
+			>&2 echo "No more auth method to try(VAULT_TOKEN, APPROLE)"
+				exit 251
+		fi
+	fi
+else
+	>&2 echo "please specify VAULT_ADDR"
+	exit 252
 fi
 
-function walk {
+function walk(){
 	_keys=$(vault kv list -format=json "${1}" | jq --raw-output '.[]')
 
 	for _key in ${_keys}; do
@@ -17,18 +34,18 @@ function walk {
 	done
 }
 
-# if return 0 2>/dev/null ; then
-# 	export -f send
-# 	return 0
-# fi
-
-if return 0 2>/dev/null; then
+if (return 0 2>/dev/null); then
 	export -f walk
 	return 0
 fi
 
-# _kv_secrets=$(vault secrets list -format=json | jq --raw-output '. | to_entries[] | select(.value.type == "kv")|.key')
+# get KV type secret engines
+KV_SE=$(vault secrets list -format=json | jq --raw-output '. | to_entries[] | select(.value.type == "kv")|.key')
 
-#for _kv in ${_kv_secrets}; do
-#	walk "${_kv}"
-#done
+ERR=$?
+
+if [ "$ERR" -eq 0 ]; then
+	for kv in ${KV_SE}; do
+		walk "${kv}"
+	done
+fi
